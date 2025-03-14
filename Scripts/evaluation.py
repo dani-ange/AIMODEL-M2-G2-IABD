cb0fdc01f1b7ec8bbfcf252153cea888b6327a56
Ngoue David Roger Yannick <david.ngoue@outlook.fr>
2025-03-14 02:37:29 +0100

    Correction des fichiers eval

--- Scripts/evaluation.py
+++ Scripts/evaluation.py
@@ -11,7 +11,7 @@ class Evaluator:
     def __init__(self):
         """Charge les ensembles de test et les modèles sauvegardés."""
         self.x_test = np.load('data/x_test.npy')
-        self.y_test = np.load('../data/y_test.npy')
+        self.y_test = np.load('data/y_test.npy')
         self.models = {
             'svm': joblib.load('model/svm_model.pkl'),
             'knn': joblib.load('model/knn_model.pkl')

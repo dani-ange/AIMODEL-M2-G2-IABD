29aac15ae2150da9674bb69afe4910d6c9882220
Ngoue David Roger Yannick <david.ngoue@outlook.fr>
2025-03-14 02:51:52 +0100

    Correction des fichiers de mail

--- Scripts/send_email.py
+++ Scripts/send_email.py
@@ -9,22 +9,24 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class EmailSender:
-    def __init__(self, smtp_user, smtp_pass, recipient, subject, body):
+    def __init__(self, smtp_user, smtp_pass, recipients, subject, body):
         """
         Initialise l'EmailSender avec les informations d'authentification et de contenu.
 
         :param smtp_user: Adresse e-mail de l'expéditeur
         :param smtp_pass: Mot de passe SMTP ou token d'authentification
-        :param recipient: Adresse e-mail du destinataire
+        :param recipients: Liste des adresses e-mail des destinataires
         :param subject: Sujet de l'email
         :param body: Corps du message
         """
         self.smtp_user = smtp_user
         self.smtp_pass = smtp_pass
-        self.recipient = recipient
+        self.recipients = recipients
         self.subject = subject
         self.body = body
 
@@ -32,34 +34,38 @@ class EmailSender:
         """Envoie l'email avec une pièce jointe."""
         msg = MIMEMultipart()
         msg['From'] = self.smtp_user
-        msg['To'] = self.recipient
+        msg['To'] = ', '.join(self.recipients)  # Utilise une chaîne séparée par des virgules
         msg['Subject'] = self.subject
 
         msg.attach(MIMEText(self.body, 'plain'))
 
-        # Ajout de la pièce jointe
-        with open(attachment_path, 'rb') as attachment:
-            part = MIMEBase('application', 'octet-stream')
-            part.set_payload(attachment.read())
-            encoders.encode_base64(part)
-            part.add_header('Content-Disposition', f'attachment; filename={os.path.basename(attachment_path)}')
-            msg.attach(part)
+        try:
+            with open(attachment_path, 'rb') as attachment:
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition',
+                               f'attachment; filename={os.path.basename(attachment_path)}')
+                msg.attach(part)
+        except FileNotFoundError:
+            logging.error(f"Fichier joint introuvable : {attachment_path}")
+            return
 
         try:
             with smtplib.SMTP('smtp.gmail.com', 587) as server:
                 server.starttls()
                 server.login(self.smtp_user, self.smtp_pass)
                 server.send_message(msg)
-                print(f"Email envoyé avec succès à {self.recipient}!")
+                logging.info(f"Email envoyé avec succès à {', '.join(self.recipients)}!")
         except Exception as e:
-            print(f"Erreur lors de l'envoi de l'email: {e}")
-
+            logging.error(f"Erreur lors de l'envoi de l'email : {e}")
 
 if __name__ == "__main__":
+    recipients = os.getenv('GROUP_EMAILS').split(',') # transforme la chaine de caractere en liste.
     sender = EmailSender(
-        smtp_user=os.getenv('SMTP_USER'),
-        smtp_pass=os.getenv('SMTP_PASS'),
-        recipient="destinataire@example.com",
+        smtp_user=os.getenv('SMTP_USER', "ngouedavidrogeryannick@gmail.com"),
+        smtp_pass=os.getenv('SMTP_PASS', "Angeline-2007???"),
+        recipients="ngouedavidroger@icloud.com",
         subject="Modèle et Documentation",
         body="Veuillez trouver ci-joint le modèle et la documentation générés."
     )
